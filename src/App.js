e71a1e2076993047db7ce79d246833db4c6382ea
Nour <nouramrine@gmail.com>
2025-10-28 12:31:37 +0100

    pull request

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ app.use((req, res) => {
   res.status(404).json({ message: "Route non trouvée" });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
