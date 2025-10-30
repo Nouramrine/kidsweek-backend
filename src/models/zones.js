b66fd749480a1a3d927c72ef8c8b241fc3604e34
aquick05 <aurelien05@gmail.com>
2025-10-30 11:56:05 +0100

    zone

--- src/models/zones.js
+++ src/models/zones.js
@@ -3,9 +3,9 @@ const { Schema } = mongoose;
 
 const zoneSchema = new Schema({
   name: { type: String, required: true },
-  color: { type: String }, 
-  owner: { type: Schema.Types.ObjectId, ref: "Members" },
-  members: [{ type: Schema.Types.ObjectId, ref: "Members" }]
+  color: { type: String },
+  owner: { type: Schema.Types.ObjectId, ref: "members" },
+  members: [{ type: Schema.Types.ObjectId, ref: "members" }],
 });
 
 const Zone = mongoose.model("zones", zoneSchema);
